dce10b9be9db00cad0603525f892950b4ebd7d34
omrobbie <putu.roby@yahoo.com>
2017-04-24 18:33:48 +0800

    modifikasi tampilan page

--- page.php
+++ page.php
@@ -4,7 +4,18 @@
           if(have_posts()):
             while(have_posts()):
               the_post();
-              the_content();
+        ?>
+              <article>
+                <header class="page-header">
+                  <h1 class="page-title">
+                    <?php the_title(); ?>
+                  </h1>
+                </header>
+                <section class="entry-content">
+                  <?php the_content(); ?>
+                </section>
+              </article>
+        <?php
             endwhile;
           endif;
         ?>
