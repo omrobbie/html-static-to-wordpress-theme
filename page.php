d18f3282cfdea2918d0e0c00e57e3369da94275f
omrobbie <putu.roby@yahoo.com>
2017-04-24 20:00:57 +0800

    pisahkan content dari page

--- page.php
+++ page.php
@@ -4,18 +4,7 @@
           if(have_posts()):
             while(have_posts()):
               the_post();
-        ?>
-              <article>
-                <header class="page-header">
-                  <h1 class="page-title">
-                    <?php the_title(); ?>
-                  </h1>
-                </header>
-                <section class="entry-content">
-                  <?php the_content(); ?>
-                </section>
-              </article>
-        <?php
+              get_template_part('content');
             endwhile;
           endif;
         ?>
