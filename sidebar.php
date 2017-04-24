43d43dbb317ccd26e79e25ab9c872d865fd00249
omrobbie <putu.roby@yahoo.com>
2017-04-24 20:03:15 +0800

    merapikan coding template

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-      <section id="sidebar">
-        <div id="primary" class="widget-area">
-          subnavi
-        </div><!-- #primary end -->
-        <div id="secondary" class="widget-area">
-          facebook like box
-        </div><!-- #secondary end -->
-      </section><!-- #sidebar end -->
+<section id="sidebar">
+  <div id="primary" class="widget-area">
+    subnavi
+  </div><!-- #primary end -->
+  <div id="secondary" class="widget-area">
+    facebook like box
+  </div><!-- #secondary end -->
+</section><!-- #sidebar end -->
