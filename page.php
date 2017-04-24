a135897e5e5548c8aa26fba7c9319b104853b2bc
omrobbie <putu.roby@yahoo.com>
2017-04-24 18:24:58 +0800

    tampilkan content dari page atau post

--- page.php
+++ page.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
       <section id="contents">
+        <?php
+          if(have_posts()):
+            while(have_posts()):
+              the_post();
+              the_content();
+            endwhile;
+          endif;
+        ?>
       </section><!-- #contents end -->
       <?php get_sidebar(); ?>
     <?php get_footer(); ?>
