9aef46644f2aace0d6990083196acdfba27e1d0e
omrobbie <putu.roby@yahoo.com>
2017-04-24 16:37:01 +0800

    pisahkan top sidebar dari index

---
+++ sidebar-top.php
@@ -0,0 +1,67 @@
+      <section id="sidebar">
+        <aside class="rss_link">
+          <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/btn_rss_feed.png" width="250" height="28" alt="RSS" /></a>
+        </aside>
+        <div id="primary" class="widget-area">
+          <aside id="event-info" class="news-list">
+            <h1>EVENT</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">event-info_title_1</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">event-info_title_2</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">EVENT LIST</a></span>
+            </div><!-- .info-wrap end -->
+          </aside><!-- #event-info end -->
+          <aside id="malls-info" class="news-list">
+            <h1>MALLS</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">malls-info_title_1</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">malls-info_title_2</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">MALLS LIST</a></span>
+            </div><!-- #info-wrap end -->
+          </aside><!-- #malls-info end -->
+          <aside id="information-info" class="news-list">
+            <h1>INFORMATION</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">information-info_title_1</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png" alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">entry-date</time>
+                  <h2><a href="#">information-info_title_2</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/post_image.png"  alt="post_image" /></a>
+                  <p>isi artikel isi artikel isi artikel isi artikel isi artikel isi artikel...</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">INFORMATION LIST</a></span>
+            </div><!-- .info-wrap end -->
+          </aside><!-- #information-info end -->
+        </div><!-- #primary end -->
+      </section><!-- #sidebar end -->
