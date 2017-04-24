f63901663346445257ce4cd975e0b04864fea711
omrobbie <putu.roby@yahoo.com>
2017-04-24 20:25:07 +0800

    modifikasi content khusus untuk archive

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
         if(have_posts()):
           while(have_posts()):
             the_post();
-            get_template_part('content');
+            get_template_part('content-archive');
           endwhile;
         endif;
       ?>
