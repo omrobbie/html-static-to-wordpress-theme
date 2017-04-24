113176d5e81e4de9e2b64028e6a5920f8c73a07c
omrobbie <putu.roby@yahoo.com>
2017-04-24 16:52:54 +0800

    ubah nama copyright di footer sesuai dengan nama site

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   </div><!-- #container end -->
   <div id="footer-container">
     <footer id="footer">
-      <p id="copyright"><small>Copyright &copy; [ company's name ] All rights reserved.</small></p>
+      <p id="copyright"><small>Copyright &copy; <?php bloginfo('name'); ?> All rights reserved.</small></p>
     </footer><!-- #footer end -->
   </div><!-- #footer-container end -->
 </div><!-- #wrap end -->
