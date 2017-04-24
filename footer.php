21b1e760a3b0c0566bb747d1f9906b44464f2ad8
omrobbie <putu.roby@yahoo.com>
2017-04-24 16:39:04 +0800

    pisahkan footer dari index

---
+++ footer.php
@@ -0,0 +1,10 @@
+    </section><!-- #contents-body end -->
+  </div><!-- #container end -->
+  <div id="footer-container">
+    <footer id="footer">
+      <p id="copyright"><small>Copyright &copy; [ company's name ] All rights reserved.</small></p>
+    </footer><!-- #footer end -->
+  </div><!-- #footer-container end -->
+</div><!-- #wrap end -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -42,13 +42,4 @@
         </section><!-- #latest-columns end -->
       </section><!-- #contents end -->
       <?php get_sidebar('top'); ?>
-    </section><!-- #contents-body end -->
-  </div><!-- #container end -->
-  <div id="footer-container">
-    <footer id="footer">
-      <p id="copyright"><small>Copyright &copy; [ company's name ] All rights reserved.</small></p>
-    </footer><!-- #footer end -->
-  </div><!-- #footer-container end -->
-</div><!-- #wrap end -->
-</body>
-</html>
+    <?php get_footer(); ?>
